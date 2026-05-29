b06f94901ce6c068eb2963274d1313671ec8a6eb
Shams <shamsudin279@gmail.com>
2026-05-29 14:07:15 +0400

    Fix models package import with fallback

--- Phase3_development/models/__init__.py
+++ Phase3_development/models/__init__.py
@@ -2,6 +2,10 @@
 Models package - Phishing Detection Models and Components
 """
 
-from .detector import PhishingDetector
+try:
+    from .detector import PhishingDetector
+except ImportError:
+    # Fallback if detector module can't be imported
+    from detector import PhishingDetector
 
 __all__ = ['PhishingDetector']
